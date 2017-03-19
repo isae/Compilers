fcd73453b874e92e2dd3172dab5fcd40f4cd8d71
iisaev <iisaev@solanteq.com>
2017-03-19 18:45:59 +0300

    added stackmachine skeleton

--- src/main/kotlin/ru/ifmo/ctddev/isaev/StackMachine.kt
+++ src/main/kotlin/ru/ifmo/ctddev/isaev/StackMachine.kt
@@ -1,5 +1,42 @@
 package ru.ifmo.ctddev.isaev
 
-fun runStackMachine(node: Node): Int {
-    return 0
+sealed class StackOp {
+    class Read : StackOp()
+    class Write : StackOp()
+    class Nop : StackOp()
+    class Push(val arg: Int) : StackOp()
+    class Ld(val arg: String) : StackOp()
+    class St(val arg: String) : StackOp()
+    class Binop(val arg: String) : StackOp()
+}
+
+fun compile(node: Node): List<StackOp> {
+    val result = ArrayList<StackOp>()
+    compile(node, result)
+    return result
+}
+
+fun compile(node: Node, stack: List<StackOp>) {
+}
+
+fun runStackMachine(operations: List<StackOp>) {
+    val stack = ArrayList<Int>()
+    operations.forEach {
+        when (it) {
+            is StackOp.Read -> {
+            }
+            is StackOp.Write -> {
+            }
+            is StackOp.Nop -> {
+            }
+            is StackOp.Push -> {
+            }
+            is StackOp.Ld -> {
+            }
+            is StackOp.St -> {
+            }
+            is StackOp.Binop -> {
+            }
+        }
+    }
 }
