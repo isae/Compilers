d7de5849cd7b1e3ff21bafd0150aca5f73520c94
iisaev <iisaev@solanteq.com>
2017-05-14 01:03:47 +0300

    added same testing for stackmachine

--- src/test/kotlin/ru/ifmo/ctddev/isaev/CoreTest.kt
+++ src/test/kotlin/ru/ifmo/ctddev/isaev/CoreTest.kt
@@ -18,6 +18,16 @@ class CoreTest {
                 .forEach { testInterpreterOnFile(it) }
     }
 
+    @Test
+    fun testStm() {
+        File("./compiler-tests/core")
+                .listFiles()
+                .filter { it.name.startsWith("test0") }
+                .map { it.nameWithoutExtension }
+                .distinct()
+                .forEach { testStmOnFile(it) }
+    }
+
     fun testInterpreterOnFile(fileName: String) {
         val expr = File("./compiler-tests/core/$fileName.expr").readText()
         val input = File("./compiler-tests/core/$fileName.input").readText()
@@ -30,6 +40,23 @@ class CoreTest {
         val outputStream = ByteArrayOutputStream(256)
         val outputWriter = PrintWriter(OutputStreamWriter(outputStream))
         Interpreter(inputReader, outputWriter).run(compiledProgram)
+        val actualOutput = String(outputStream.toByteArray())
+        Assert.assertEquals("Expected different output", expectedOutput, actualOutput)
+    }
+
+    fun testStmOnFile(fileName: String) {
+        val expr = File("./compiler-tests/core/$fileName.expr").readText()
+        val input = File("./compiler-tests/core/$fileName.input").readText()
+        val expectedOutput = File("./compiler-tests/core/orig/$fileName.log").readText()
+        println(expr)
+        println(input)
+        println(expectedOutput)
+        val programAST = buildAST(expr)
+        val compiledSTM = compile(programAST)
+        val inputReader = BufferedReader(StringReader(input))
+        val outputStream = ByteArrayOutputStream(256)
+        val outputWriter = PrintWriter(OutputStreamWriter(outputStream))
+        StackMachine(inputReader, outputWriter).run(compiledSTM)
         val actualOutput = String(outputStream.toByteArray())
         Assert.assertEquals("Expected different output", expectedOutput, actualOutput)
     }
