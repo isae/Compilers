0354dff1b26c46b10884f1905e6a2b808d3c23b1
iisaev <iisaev@solanteq.com>
2017-05-28 14:14:38 +0300

    minor

--- compile.sh
+++ compile.sh
@@ -6,5 +6,5 @@ echo "Working directory ${WORKING_DIR}"
 FILENAME=$(basename "$1")
 FILENAME="${FILENAME%.*}"
 FILENAME=${OUT}/${FILENAME}
-nasm -f macho ${OUT}/${name}.asm -o ${FILENAME}.o  && gcc -arch i386 -o ${FILENAME} ${FILENAME}.o
+nasm -f macho -g ${OUT}/${name}.asm -o ${FILENAME}.o  && gcc -arch i386 -m32 -g -O0 -o ${FILENAME} ${FILENAME}.o
 ${FILENAME}
