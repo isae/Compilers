504366dcd8be944ba3957b1aa34650b14aa411de
iisaev <iisaev@solanteq.com>
2017-05-28 15:11:51 +0300

    compile sh

--- compile.sh
+++ compile.sh
@@ -6,5 +6,5 @@ echo "Working directory ${WORKING_DIR}"
 FILENAME=$(basename "$1")
 FILENAME="${FILENAME%.*}"
 FILENAME=${OUT}/${FILENAME}
-nasm -f macho -g ${OUT}/${name}.asm -o ${FILENAME}.o  && gcc -arch i386 -m32 -g -O0 -o ${FILENAME} ${FILENAME}.o
+nasm -f elf32 -F dwarf -g ${OUT}/${name}.asm -o ${FILENAME}.o  && gcc -m32 -g -O0 -o ${FILENAME} ${FILENAME}.o
 ${FILENAME}
