ba178f468e6489c5e770fa341f6be8d4bb779127
iisaev <iisaev@solanteq.com>
2017-06-07 00:26:29 +0300

    preserving of register values during function calls

--- src/main/kotlin/ru/ifmo/ctddev/isaev/Compiler.kt
+++ src/main/kotlin/ru/ifmo/ctddev/isaev/Compiler.kt
@@ -10,10 +10,10 @@ val prefix = """
 read_int:
   enter $16, $0
   andl $-16, %esp
-  lea int_read, %eax
-  movl %eax, 4(%esp)
-  lea format_in, %eax
-  movl %eax, (%esp)
+  lea int_read, %ebx
+  movl %ebx, 4(%esp)
+  lea format_in, %ebx
+  movl %ebx, (%esp)
   call scanf
   movl $0, %eax
   leave
@@ -22,10 +22,10 @@ read_int:
 write_int:
   enter $16, $0
   andl $-16, %esp
-  movl 8(%ebp), %eax
-  movl %eax, 4(%esp)
-  lea format_out, %eax
-  movl %eax, (%esp)
+  movl 8(%ebp), %ebx
+  movl %ebx, 4(%esp)
+  lea format_out, %ebx
+  movl %ebx, (%esp)
   call printf
   movl $0, %eax
   leave
@@ -34,12 +34,12 @@ write_int:
 debug_int:
   enter $16, $0
   andl $-16, %esp
-  movl 8(%ebp), %eax
-  movl %eax, 4(%esp)
-  lea debug_out, %eax
-  movl %eax, (%esp)
+  movl 8(%ebp), %ebx
+  movl %ebx, 4(%esp)
+  lea debug_out, %ebx
+  movl %ebx, (%esp)
   call printf
-  movl $0, %eax
+  movl $0, %ebx
   leave
   ret
 
@@ -59,8 +59,11 @@ ret
 """
 
 val COMM_PREFIX = "//"
-val debugEnabled = true
+val debugEnabled = false
 
+// %eax - for return value
+// %ebx - for using in int_read and int_write
+// %ecx, %edx - for intermediate operations
 
 fun compile(nodes: List<StackOp>): List<String> {
     val ops = ArrayList<String>()
@@ -98,9 +101,6 @@ private fun compile(op: StackOp, ops: MutableList<String>, varOffsets: HashMap<S
         }
         is StackOp.Label -> {
             ops += "${op.label}:"
-            if (op.label == MAIN_NAME) {
-                ops += "xorl %ecx, %ecx"
-            }
         }
         is StackOp.Comm -> {
             ops /= "$COMM_PREFIX${op.comment}"
@@ -114,48 +114,48 @@ private fun compile(op: StackOp, ops: MutableList<String>, varOffsets: HashMap<S
             ops /= "pushl $offset(%ebp) // Load ${op.arg}"
         }
         is StackOp.St -> {
-            ops /= "popl %eax"
+            ops /= "popl %ecx"
             val offset = varOffsets[op.arg] ?: throw IllegalStateException("Variable not defined: ${op.arg}")
-            ops /= "movl %eax, $offset(%ebp) // Store ${op.arg}"
+            ops /= "movl %ecx, $offset(%ebp) // Store ${op.arg}"
         }
         is StackOp.Binop -> {
             when (op.op) {
                 "*" -> {
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "popl %edx"
                     ops /= "mull %edx"
-                    ops /= "pushl %eax"
+                    ops /= "pushl %ecx"
                 }
                 "+" -> {
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "popl %edx"
-                    ops /= "addl %edx, %eax"
-                    ops /= "pushl %eax"
+                    ops /= "addl %edx, %ecx"
+                    ops /= "pushl %ecx"
                 }
                 "-" -> {
                     ops /= "popl %edx"
-                    ops /= "popl %eax"
-                    ops /= "subl %edx, %eax"
-                    ops /= "pushl %eax"
+                    ops /= "popl %ecx"
+                    ops /= "subl %edx, %ecx"
+                    ops /= "pushl %ecx"
                 }
                 "/" -> {
                     ops /= "xorl %edx, %edx"
                     ops /= "popl %ecx"
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "divl %ecx"
-                    ops /= "pushl %eax"
+                    ops /= "pushl %ecx"
                 }
                 "%" -> {
                     ops /= "xorl %edx, %edx"
                     ops /= "popl %ecx"
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "divl %ecx"
                     ops /= "pushl %edx"
                 }
                 "<" -> {
                     ops /= "popl %edx"
-                    ops /= "popl %eax"
-                    ops /= "cmp %edx, %eax" //compare and set flags
+                    ops /= "popl %ecx"
+                    ops /= "cmp %edx, %ecx" //compare and set flags
                     ops /= "jl .+6"
                     ops /= "pushl \$0"
                     ops /= "jmp .+4"
@@ -163,44 +163,44 @@ private fun compile(op: StackOp, ops: MutableList<String>, varOffsets: HashMap<S
                 }
                 "<=" -> {
                     ops /= "popl %edx"
-                    ops /= "popl %eax"
-                    ops /= "cmp %edx, %eax" //compare and set flags
+                    ops /= "popl %ecx"
+                    ops /= "cmp %edx, %ecx" //compare and set flags
                     ops /= "jle .+6"
                     ops /= "pushl \$0"
                     ops /= "jmp .+4"
                     ops /= "pushl \$1"
                 }
                 ">" -> {
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "popl %edx"
-                    ops /= "cmp %edx, %eax" //compare and set flags
+                    ops /= "cmp %edx, %ecx" //compare and set flags
                     ops /= "jl .+6"
                     ops /= "pushl \$0"
                     ops /= "jmp .+4"
                     ops /= "pushl \$1"
                 }
                 ">=" -> {
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "popl %edx"
-                    ops /= "cmp %edx, %eax" //compare and set flags
+                    ops /= "cmp %edx, %ecx" //compare and set flags
                     ops /= "jle .+6"
                     ops /= "pushl \$0"
                     ops /= "jmp .+4"
                     ops /= "pushl \$1"
                 }
                 "==" -> {
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "popl %edx"
-                    ops /= "cmp %edx, %eax" //compare and set flags
+                    ops /= "cmp %edx, %ecx" //compare and set flags
                     ops /= "je .+6"
                     ops /= "pushl \$0"
                     ops /= "jmp .+4"
                     ops /= "pushl \$1"
                 }
                 "!=" -> {
-                    ops /= "popl %eax"
+                    ops /= "popl %ecx"
                     ops /= "popl %edx"
-                    ops /= "cmp %edx, %eax" //compare and set flags
+                    ops /= "cmp %edx, %ecx" //compare and set flags
                     ops /= "jne .+6"
                     ops /= "pushl \$0"
                     ops /= "jmp .+4"
@@ -210,8 +210,8 @@ private fun compile(op: StackOp, ops: MutableList<String>, varOffsets: HashMap<S
             }
         }
         is StackOp.Jif -> {
-            ops /= "popl %eax"
-            ops /= "cmp $0, %eax"
+            ops /= "popl %ecx"
+            ops /= "cmp $0, %ecx"
             ops /= "je ${op.label}"
         }
         is StackOp.Jump -> {
@@ -228,18 +228,26 @@ private fun compile(op: StackOp, ops: MutableList<String>, varOffsets: HashMap<S
                 varOffsets[it] = argOffset
                 argOffset += 4
             }
-            var localVarOffset = -4
+            var localVarOffset = -20 // preserving of eax, ebx, ecx, edx
             op.localVariables.reversed().forEach {
                 varOffsets[it] = localVarOffset
                 localVarOffset -= 4
             }
-            val frameSize = (op.localVariables.size + 1) * 4
+            val frameSize = (op.localVariables.size + 5) * 4
             ops /= "enter \$$frameSize, $0"
+            ops /= "mov %eax, -4(%ebp)"
+            ops /= "mov %ebx, -8(%ebp)"
+            ops /= "mov %ecx, -12(%ebp)"
+            ops /= "mov %edx, -16(%ebp)"
         }
         is StackOp.Ret -> {
             if (debugEnabled) {
                 ops /= "call debug_int // Debug return value that is on stack"
             }
+            ops /= "mov -4(%ebp), %eax"
+            ops /= "mov -8(%ebp), %ebx"
+            ops /= "mov -12(%ebp), %ecx"
+            ops /= "mov -16(%ebp), %edx"
             ops /= "popl %eax"
             ops /= "leave"
             ops /= "ret\n"
